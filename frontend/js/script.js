951f73b49b25136218abe7f6f308a15fdc074181
Your Name <ph929551@gmail.com>
2023-11-16 02:15:27 -0300

    inicial commit

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -81,7 +81,7 @@ const handleLogin = (event) => {
     user.color = getRandomColor()
     login.style.display = "none"
     chat.style.display = "flex"
-    websocket = new WebSocket("ws://localhost:8080")
+    websocket = new WebSocket("wss://zap-bomdasdsa.onrender.com")
     websocket.onmessage = processMessage
 }
 
